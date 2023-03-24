5ef38d188879517e64ed466a672d8cdddc9b5551
Piyush <piyush2016amla@gmail.com>
2023-03-24 23:42:19 +0530

    first commit

---
+++ src/Components/Tours.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Card from './Card'
+
+
+export default function Tours({tours, removeTour}) {
+  return (
+    <div className="container">
+
+        <div className="title">
+            <h2> Plan With Love</h2>
+        </div>
+
+        <div className="cards">
+            {
+                    tours.map( (tour) => {
+                        return <Card key={tour.id} {...tour} removeTour={removeTour}></Card>
+                    })
+            }
+        </div>
+        
+    </div>
+  );
+}
